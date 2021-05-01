a27bb100d34c6c7b3dd4070af312d0c37f9d7a2c
Munni26 <tazneen99@gmail.com>
2021-05-01 10:19:32 +0600

    44 first commit

--- src/App.js
+++ src/App.js
@@ -5,11 +5,31 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import Home from './components/Home/Home';
+import Login from './components/Login/Login';
+import Book from './components/Book/Book';
+import Header from './components/Header/Header';
 
 
 function App() {
   return (
-    
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/home">
+          <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/book/:bedType">
+          <Book />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
