63c73c33e17fb388d8e66f3cc1a7e39139bfd195
Munni26 <tazneen99@gmail.com>
2021-05-04 08:36:54 +0600

    44 last commit

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useParams } from 'react-router';
+import { Link } from 'react-router-dom';
 
 const Book = () => {
+    const { bedType } = useParams();
     return (
-        <div>
-            
+        <div style={{ textAlign: 'center' }}>
+            <h1>Let's book a {bedType} Room.</h1>
+            <p>Want a <Link to="/home">different room?</Link> </p>
         </div>
     );
 };
